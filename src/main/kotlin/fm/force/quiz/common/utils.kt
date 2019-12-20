a97e3e08b21cb10712fc9a910e5985fb9a8bc6aa
night-crawler <lilo.panic@gmail.com>
2019-12-21 01:56:52 +0300

    wip

--- src/main/kotlin/fm/force/quiz/common/utils.kt
+++ src/main/kotlin/fm/force/quiz/common/utils.kt
@@ -1,5 +1,10 @@
 package fm.force.quiz.common
 
+import java.time.Instant
+import java.util.*
+import kotlin.math.log2
+import kotlin.math.nextUp
+
 val ALPHABET = ('a'..'z') + ('A'..'Z') + ('0'..'9')
 
 
@@ -8,3 +13,38 @@ fun getRandomString(len: Int = 8) =
                 .map { kotlin.random.Random.nextInt(0, ALPHABET.size) }
                 .map(ALPHABET::get)
                 .joinToString("")
+
+fun Long.toBinaryString(): String = BitSet.valueOf(longArrayOf(this)).let { bs ->
+    (63 downTo 0).joinToString("") {
+        if (bs[it]) "1" else "0"
+    }
+}
+
+fun Long.mostSignificantBit(): Int {
+    var count = 0
+    var n = this
+
+    while(n != 0L && n != -1L) {
+        n = n shr 1
+        count++
+        if (count > 100)
+            break
+    }
+    return count
+}
+
+fun Long.maxSignificant(): Long {
+    return (1L shl mostSignificantBit()) - 1
+}
+
+class IdGenerator {
+    companion object {
+        fun valueOf(ts: Instant) {
+
+        }
+    }
+    fun generate() {
+        val a = System.currentTimeMillis()
+        val q = Date(a)
+    }
+}
--- src/test/kotlin/fm/force/quiz/common/UtilsKtTest.kt
+++ src/test/kotlin/fm/force/quiz/common/UtilsKtTest.kt
@@ -1,14 +1,36 @@
 package fm.force.quiz.common
 
 import io.kotlintest.matchers.string.shouldHaveLength
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.*
+import java.time.Instant
 
 
 open class UtilsKtTest : WordSpec({
     "getRandomString" should {
         "generate random string" {
             val str = getRandomString(8)
-            str.shouldHaveLength(8)
+            str shouldHaveLength 8
         }
+    }
+
+    "toBinaryString" should {
+        "produce correct length" {
+            val one = 1L
+            one.toBinaryString() shouldHaveLength 64
+
+            Long.MAX_VALUE.toBinaryString() shouldHaveLength 64
+            Long.MAX_VALUE.toBinaryString().count { it == '1' } shouldBe 63
+            Long.MAX_VALUE.toBinaryString().count { it == '0' } shouldBe 1
+
+            Long.MIN_VALUE.toBinaryString() shouldHaveLength 64
+            Long.MIN_VALUE.toBinaryString().count { it == '1' } shouldBe 1
+            Long.MIN_VALUE.toBinaryString().count { it == '0' } shouldBe 63
+        }
+    }
+
+    "qwe" should {
+        var ts = Instant.now()
+        ts.toEpochMilli()
     }
 })
